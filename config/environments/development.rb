6aab65894fb2e3e4416ac0c15eb60ab3a49d99fe
mfebrianto <michaelfebrianto@gmail.com>
2014-09-12 00:29:32 +1000

    master : dashboard header

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.assets.paths << Rails.root.join("app", "assets", "stylesheets", "plugins", "bootstrap", "css")
+  config.assets.paths << Rails.root.join("app", "assets", "icons", "font-awesome", "css")
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += ['plugins/bootstrap/css/bootstrap.min.css']
+Rails.application.config.assets.precompile += ['font-awesome.min.css']
+Rails.application.config.assets.precompile += ['style.css']
+Rails.application.config.assets.precompile += ['plugins.css']
+Rails.application.config.assets.precompile += ['demo.css']
