a94ab8f47075bf80bbfcd42d4fc5e493d8d03fd7
mfebrianto <michaelfebrianto@gmail.com>
2014-09-21 02:00:08 +1000

    master : use bootstrap frontpage

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += ['plugins/bootstrap/css/bootstrap.min.css']
+Rails.application.config.assets.precompile += ['frontpage/bootstrap.min.css']
 Rails.application.config.assets.precompile += ['bootstrap-responsive.min.css']
 Rails.application.config.assets.precompile += ['font-awesome.min.css']
 Rails.application.config.assets.precompile += ['style.css']
