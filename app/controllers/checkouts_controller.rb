e70bac3ffe2c92095f6680abf5fd417e82d24eec
mfebrianto <michaelfebrianto@gmail.com>
2014-10-21 19:05:32 +1100

    master : ready for accepts_nested_attributes_for

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -8,4 +8,19 @@ class CheckoutsController < FreeController
     @customer.addresses.build # if it is not built first then it will return empty object.
   end
 
+
+  def create
+
+    @customer = Customer.new(checkouts_params)
+
+    Rails.logger.info ">>>>>>>>>>#{@customer.inspect}"
+    Rails.logger.info ">>>>>>>>>>#{@customer.addresses.inspect}"
+  end
+
+  private
+
+  def checkouts_params
+    params.require(:customer).permit(:name, :email, addresses_attributes: [:address, :id])
+  end
+
 end
