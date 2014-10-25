5dbf2905f97cab99f3e6e2858323f8312c9c38f7
mfebrianto <michaelfebrianto@gmail.com>
2014-10-25 14:55:26 +1100

    master : validate email on customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,4 +8,6 @@ class Customer < ActiveRecord::Base
 
   validates_presence_of :name, :email, :phone
 
+  validates_format_of :email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,4 +6,12 @@ describe Customer do
   it { should validate_presence_of :email }
   it { should validate_presence_of :phone }
 
+  it 'should return error on invalid email' do
+    invalid_email = %w(notanemail notanemail.com not@anemail)
+    invalid_email.each do |invalid_email|
+      customer = Customer.new(name: 'testname', phone: 'testphone', email: invalid_email)
+      expect(customer.valid?).to be_falsey
+    end
+  end
+
 end
