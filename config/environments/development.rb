8c1cb70e2b869dc8929079b228b8b7be32266505
mfebrianto <michaelfebrianto@gmail.com>
2014-10-27 12:40:22 +1100

    master : fastmail configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,10 +41,16 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :address => "192.168.186.161",
-      :port => 25,
-      :domain => "example.com",
+      address: 'mail.messagingengine.com',
+      port: 465,
+      domain: 'fancybite.com',
+      authentication: :plain,
+      enable_starttls_auto: false,
+      ssl: true,
+      user_name: 'fancybite@fastmail.com',
+      password: 'MfC221686!'
   }
 end
