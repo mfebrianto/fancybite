ae49851d37263be6ac368235af0b5645f73fd23c
mfebrianto <michaelfebrianto@gmail.com>
2014-12-18 08:22:39 +1100

    master : update reciept

--- app/models/order.rb
+++ app/models/order.rb
@@ -28,14 +28,21 @@ class Order < ActiveRecord::Base
   end
 
   def generate_pdf
-    pdf = Prawn::Document.new(page_size: [200,600], margin: [10, 5, 5, 5])
+    pdf = Prawn::Document.new(page_size: [200,480], margin: [10, 5, 5, 5])
     pdf.image Rails.root.join('app','assets','images','logo','logobw.jpg'), scale: 0.2, :position => :center
-    pdf.line [10,490], [290,490]
+    pdf.draw_text 'Your Order No : ', at: [40, 370], size: 8
+    pdf.fill_color 'ff6bc2'
+    pdf.draw_text transfer_key, at: [120, 370], size: 8
+    pdf.stroke do
+      pdf.fill_color '505050'
+      pdf.horizontal_line(5, 190, at: 360)
+    end
     order_detail.each do |od|
-      pdf.draw_text od.quantity.to_s, at: [2, 480], size: 10
-      pdf.draw_text od.menu_item.name, at: [20, 480], size: 10
-      pdf.draw_text od.menu_item.price.to_s, at: [100, 480], size: 10
-      pdf.draw_text (od.menu_item.price * od.quantity).to_s, at: [150, 480], size: 10
+      pdf.fill_color '505050'
+      pdf.draw_text od.quantity.to_s, at: [5, 350], size: 8
+      pdf.draw_text od.menu_item.name, at: [20, 350], size: 8
+      pdf.draw_text od.menu_item.price.to_s, at: [105, 350], size: 8
+      pdf.draw_text (od.menu_item.price * od.quantity).to_s, at: [155, 350], size: 8
     end
     pdf.render_file "receipts/receipt.pdf"
   end
